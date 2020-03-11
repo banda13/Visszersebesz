f9fb5c53a258256053a9dd131caa6614d887435c
Szabó András <szabag@gmail.com>
2020-03-11 20:06:32 +0100

    modularize page content, dynamic loading of the elements

--- index.php
+++ index.php
@@ -64,16 +64,16 @@
             <i class="fa fa-bars"></i>
         </a>
         <a href="#home" class="w3-bar-item w3-button">HOME</a>
-        <a href="#welcome" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-"></i>Köszöntő</a>
-        <a href="#introduction" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> Bemutatkozás</a>
-        <a href="#contact" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-phone"></i>
+        <a href="#welcome_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-"></i>Köszöntő</a>
+        <a href="#introduction_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-user"></i> Bemutatkozás</a>
+        <a href="#contact_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-phone"></i>
             Munkahelyek/elérhetőségek</a>
-        <a href="#welcome" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-book"></i> Érbetegségek –
+        <a href="#knowledge_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-book"></i> Érbetegségek –
             alapismeretek és műtétek</a>
-        <a href="#welcome" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-search"></i> Blog</a>
-        <a href="#welcome" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-users"></i> Orvos válaszol</a>
-        <a href="#welcome" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-link"></i> Hivatkozások</a>
-        <a href="#welcome" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-font"></i> Kulcsszavak</a>
+        <a href="#blog_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-search"></i> Blog</a>
+        <a href="#q_and_a_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-users"></i> Orvos válaszol</a>
+        <a href="#references_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-link"></i> Hivatkozások</a>
+        <a href="#keywords_container" class="w3-bar-item w3-button w3-hide-small"><i class="fa fa-font"></i> Kulcsszavak</a>
 
 
         <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-red">
@@ -83,9 +83,16 @@
 
     <!-- Navbar on small screens -->
     <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium">
-        <a href="#welcome" class="w3-bar-item w3-button" onclick="toggleFunction()">Köszöntő</a>
-        <a href="#introduction" class="w3-bar-item w3-button" onclick="toggleFunction()">Bemutatkozás</a>
-        <a href="#workandcontact" class="w3-bar-item w3-button" onclick="toggleFunction()">Munkahelyek/elérhetőségek</a>
+        <a href="#welcome_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Köszöntő</a>
+        <a href="#introduction_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Bemutatkozás</a>
+        <a href="#contact_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Munkahelyek/elérhetőségek</a>
+        <a href="#knowledge_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Érbetegségek –
+            alapismeretek és műtétek</a>
+        <a href="#blog_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Blog</a>
+        <a href="#q_and_a_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Orvos válaszol</a>
+        <a href="#references_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Hivatkozások</a>
+        <a href="#keywords_container" class="w3-bar-item w3-button" onclick="toggleFunction()">Kulcsszavak</a>
+
         <a href="#" class="w3-bar-item w3-button">Keresés</a>
     </div>
 </div>
@@ -97,177 +104,24 @@
     </div>
 </div>
 
-<!-- Container (Welcome Section) -->
-<div class="w3-content w3-container w3-padding-64" id="welcome">
-    <h2 class="w3-center">KÖSZÖNTŐ</h2>
-    <p>Tíz év általános sebészeti tevékenység után specializálódtam az érsebészetre, melyet húsz éve
-        gyakorlok. Munkám során az érbetegségek minden műtétet igénylő betegségét ellátom, mind a
-        hagyományos, mind a világban elérhető legmodernebb műtéti technikák rendelkezésre állnak.
-        Tervezett műtétre az ország bármely részéről érkező beteget fel tudunk venni.</p>
+    <div id="welcome_container"></div>
 
-    <div class="w3-ul">
-        <h4>Legfontosabb területek:</h4>
-        <div class="w3-li w3-margin-bottom">
-            <div class="w3-margin-left">
-                <i class="fa fa-circle w3-margin-right"></i> Nyaki erek sebészete (carotis), stroke prevenció
-            </div>
-        </div>
-        <div class="w3-li w3-margin-bottom">
-            <div class="w3-margin-left">
-                <i class="fa fa-circle w3-margin-right"></i> Hasi és alsó végtagi érszűkületes betegségek gyógyszeres és
-                műtéti kezelése
-            </div>
-        </div>
-        <div class="w3-li w3-margin-bottom">
-            <div class="w3-margin-left">
-                <i class="fa fa-circle w3-margin-right"></i> Verőértágulatok (aneurysma) nyitott és endovascularis
-                (stentgraft) kezelése az aorta mellkasi
-                és hasi szakaszán, a perifériákon
-            </div>
+    <div id="introduction_container"></div>
 
-        </div>
-        <div class="w3-li w3-margin-bottom">
-            <div class="w3-margin-left">
-                <i class="fa fa-circle w3-margin-right"></i> Visszerek nyitott műtéti és modern endovénás (lézeres,
-                radioferkvenciás, ragasztásos)
-                kezelése valamint sclerotherapia
-            </div>
-        </div>
+    <div id="contact_container"></div>
 
-        <div class="w3-ul">
-            <h4>Ambuláns betegvizsgálat és előjegyzés:</h4>
-            <div class="w3-li w3-margin-bottom">
-                <div class="w3-margin-left">
-                    <i class="fa fa-circle w3-margin-right"></i> Semmelweis Egyetem Szív- és Érgyógyászati Klinika,
-                    ambulancia:
-                    <p class="w3-margin-left">OEP rendelés, előzetes időpont-biztosítás <a href="tel:+3614586800">
-                            (+3614586800) </a> és beutaló szükséges </p>
-                </div>
-            </div>
-            <div class="w3-li w3-margin-bottom">
-                <div class="w3-margin-left">
-                    <i class="fa fa-circle w3-margin-right"></i> Budai Egészségközpont, ambulancia:
-                    <p class="w3-margin-left">Magánrendelés, előzetes időpont-biztosítás <a href="tel:+3614895200">
-                            (+3614895200) </a> szükséges </p>
-                </div>
-            </div>
-        </div>
-        <div class="w3-ul">
-            <h4>Műtétek:</h4>
-            <div class="w3-li w3-margin-bottom">
-                <div class="w3-margin-left">
-                    <i class="fa fa-circle w3-margin-right"></i> Semmelweis Egyetem Szív- és Érgyógyászati Klinika::
-                    <p class="w3-margin-left">minden artériás műtét (carotis, aneurysma, alsó végtag) és hagyományos,
-                        nyitott visszérműtétek – OEP finanszírozással </p>
-                </div>
-            </div>
-            <div class="w3-li w3-margin-bottom">
-                <div class="w3-margin-left">
-                    <i class="fa fa-circle w3-margin-right"></i> VP-MED Egészségcentrum:
-                    <p class="w3-margin-left">modern visszérműtétek (lézeres, radiofrekvenciás, ragasztásos) –
-                        magánrendelés keretében </p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <div id="knowledge_container"></div>
 
-    <!-- Container (Introduction Section) -->
-    <div class="w3-content w3-container w3-padding-64" id="introduction">
-        <h2 class="w3-center">Bemutatkozás</h2>
+    <div id="blog_container"></div>
 
-        <h4 class="w3-center">Tanulmányaim</h4>
-        <h4 class="w3-center">Munkahelyek, beosztások</h4>
-        <h4 class="w3-center">Nyelvvizsgák</h4>
-        <h4 class="w3-center">Tagságok</h4>
-        <h4 class="w3-center">Szcientriai mutatók</h4>
-        <h4 class="w3-center">Oktatás</h4>
+    <div id="q_and_a_container"></div>
 
-    </div>
+    <div id="references_container"></div>
 
-    <!-- Container (Contact Section) -->
-    <div class="w3-content w3-container w3-padding-64" id="contact">
-        <h3 class="w3-center">MUNKAHELYEK, ELÉRHETŐSÉG</h3>
+    <div id="keywords_container"></div>
 
-        <div class="w3-row w3-padding-32 w3-section">
-            <div class="w3-col m6 w3-panel">
-                <div class="w3-large w3-margin-bottom">
-                    <h4><b> Semmelweis Egyetem Szív-és Érsebészeti Klinika, Érsebészeti Tanszék</b></h4>
-                    <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Budapest, 1122
-                    Városmajor utca 68.<br>
-                    <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Ambulancia időpont:
-                    <a href="tel:+36145868800"> +36145868800</a><br>
-                    <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> <a
-                            href="mailto:info@kardio.sote.hu">info@kardio.sote.hu</a><br>
-                    <i class="fa fa-user fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Rendelési idő: kedd,
-                    9-14 óra<br>
-                    <i class="fa fa-info fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Tevékenység: <br>
-                    Ambuláns rendelés, teljes artériás érsebészeti betegségcsoport műtétei és hagyományos
-                    nyitott visszérműtét<br>
-                    <i class="fa fa-map-signs fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>
-                    Megközelítés:<br>
-                    128 vagy 129 busz és 20m séta <br>
-                    3 Metro Széll Kálmán tér és 15 perc séta <br>
-                    Gépkocsival a környéken, fizetős parkolás. <br>
-                </div>
-            </div>
-            <div class="w3-col m6 w3-panel">
-                <div class="w3-margin map" id="map_sote"></div>
-            </div>
-        </div>
 
-        <div class="w3-row w3-padding-32 w3-section">
-            <div class="w3-col m6 w3-panel">
-                <div class="w3-large w3-margin-bottom">
-                    <h4><b> Budai Egészségközpont</b></h4>
-                    <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Budapest, 1126
-                    Királyhágó utca 1-3.<br>
-                    <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Ambulancia időpont:
-                    <a href="tel:+3614895200"> +3614895200</a><br>
-                    <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>
-                    https://online.bhc.hu/Ugyfel/<br>
-                    <i class="fa fa-user fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Rendelési idő: hétfő
-                    17-20 óra<br>
-                    <i class="fa fa-info fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Tevékenység: ambuláns
-                    betegvizsgálat<br>
-                    <i class="fa fa-search-plus fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>
-                    Megközelítés:<br>
-                    Deák térről az Alagúton át (105-ös autóbusszal) Királyhágó utcai megálló<br>
-                    Újbuda irányából a Királyhágó tér felől (212-es autóbusszal) Királyhágó téri megálló<br>
-                    A Böszörményi út felől (59-es villamossal) Királyhágó téri megálló<br>
-                    Az Alkotás út felől (61-es villamossal) Királyhágó utcai megálló<br>
-                    Gépkocsival a kórház területén parkolás<br>
-                </div>
-            </div>
-            <div class="w3-col m6 w3-panel">
-                <div class="w3-margin map" id="map_buda"></div>
-            </div>
-        </div>
-
-        <div class="w3-row w3-padding-32 w3-section">
-            <div class="w3-col m6 w3-panel">
-                <div class="w3-large w3-margin-bottom">
-                    <h4><b> VP-MED Egészségcentrum és Oktatási Központ</b></h4>
-                    <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Budapest, 1042
-                    Árpád út 56, sz.33<br>
-                    <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Ambulancia időpont:
-                    <a href="tel:+36309631363"> +36309631363</a><br>
-                    <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> info@vpmed.hu<br>
-                    <i class="fa fa-info fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Tevékenység: új
-                    típusú (lézeres, radiofrekvenciás, ragazstásos) visszérműtétek végzése<br>
-                    <i class="fa fa-search-plus fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i>
-                    Megközelítés:<br>
-                    3 Metro vagy 12 v. 14 villamos Újpest–Központ és 250m séta. <br>
-                    Gépkocsival parkolás az Újpesti Piac parkolójában és 50 séta. <br>
-                </div>
-            </div>
-            <div class="w3-col m6 w3-panel">
-                <div class="w3-margin map" id="map_vpmed"></div>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- Footer -->
+<!-- Footer -->
     <footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
         <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
         <div class="w3-xlarge w3-section">
@@ -294,13 +148,27 @@
 
         $(document).ready(function () {
 
-            // Modal Image Gallery
-            function onClick(element) {
-                document.getElementById("img01").src = element.src;
-                document.getElementById("modal01").style.display = "block";
-                var captionText = document.getElementById("caption");
-                captionText.innerHTML = element.alt;
-            }
+
+            // Load modules
+            $(function(){
+                $("#welcome_container").load("html/welcome.html");
+                console.log("Welcome section loaded");
+                $("#introduction_container").load("html/introduction.html");
+                console.log("Introduction section loaded");
+                $("#contact_container").load("html/contact.html");
+                console.log("Contact section loaded");
+                $("#knowledge_container").load("html/knowledge.html");
+                console.log("Knowledge section loaded");
+                $("#blog_container").load("html/blog.html");
+                console.log("Blog section loaded");
+                $("#q_and_a_container").load("html/q_and_a.html");
+                console.log("Q and A section loaded");
+                $("#references_container").load("html/references.html");
+                console.log("References section loaded");
+                $("#keywords_container").load("html/keywords.html");
+                console.log("Keywords section loaded");
+            });
+
 
             // Change style of navbar on scroll
             window.onscroll = function () {
@@ -328,7 +196,6 @@
             }
 
             let sote_map, buda_map, vpmed_map;
-
             function initMap() {
 
                 // The location of Uluru
