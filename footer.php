2278a62a69c72fbbdd0b1e3402a9746bba91356c
Szabó András <szabag@gmail.com>
2020-04-13 09:48:50 +0200

    footer improvements

--- footer.php
+++ footer.php
@@ -5,51 +5,42 @@
         <div class="row mb-5">
             <div class="col-md">
                 <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2 logo">Dentista</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                </div>
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Have a Questions?</h2>
-                    <div class="block-23 mb-3">
-                        <ul>
-                            <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-                        </ul>
+                    <h3 class="ftco-heading-3">Kérdése van?</h3>
+                    <div class="block-23 mb-1">
+                        <a href="/kerdesek.php" class="btn btn-secondary px-4 py-3">Ingyenes üzenet</a>
                     </div>
 
-                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
-                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-5 ml-md-4">
+                    <h3 class="ftco-heading-3">Honlap térkép</h3>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Kezdőlap</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Bemutatkozás</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Érbetegségek</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Árak</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Blog</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Orvos válaszol</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Elérhetőségek</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Hivatkozások</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md">
                 <div class="ftco-footer-widget mb-5 ml-md-4">
-                    <h2 class="ftco-heading-2">Links</h2>
+                    <h2 class="ftco-heading-2">Legfontosabb szakterületek</h2>
                     <ul class="list-unstyled">
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Deparments</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
-                    </ul>
-                </div>
-                <div class="ftco-footer-widget mb-5 ml-md-4">
-                    <h2 class="ftco-heading-2">Services</h2>
-                    <ul class="list-unstyled">
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Neurolgy</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Dentist</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Ophthalmology</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Cardiology</a></li>
-                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Surgery</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Nyaki erek</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Alsó végtagi érszűkület</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Értágulatok, aneurysmák</a></li>
+                        <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Visszeresség</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md">
                 <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Recent Blog</h2>
+                    <h2 class="ftco-heading-2">Elérhetőségek</h2>
                     <div class="block-21 mb-4 d-flex">
                         <a class="blog-img mr-4" style="background-image: url(../images/image_1.jpg);"></a>
                         <div class="text">
@@ -74,28 +65,10 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md">
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Opening Hours</h2>
-                    <h3 class="open-hours pl-4"><span class="ion-ios-time mr-3"></span>We are open 24/7</h3>
-                </div>
-                <div class="ftco-footer-widget mb-5">
-                    <h2 class="ftco-heading-2">Subscribe Us!</h2>
-                    <form action="#" class="subscribe-form">
-                        <div class="form-group">
-                            <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
-                            <input type="submit" value="Subscribe" class="form-control submit px-3">
-                        </div>
-                    </form>
-                </div>
-            </div>
         </div>
         <div class="row">
             <div class="col-md-12 text-center">
-
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                <p>Dr. Szabó Gábor Viktor Ph.D Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
             </div>
         </div>
     </div>
